30ff3fee0b7c513f6126b46985ca58a14be3c6ba
Mahdi <mahdihsoumi@gmail.com>
2022-12-02 18:10:18 +0100

    feat: added axios

--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+
+app.use(VueAxios, axios);
+app.provide("axios", app.config.globalProperties.axios);
+
+app.mount("#app");
